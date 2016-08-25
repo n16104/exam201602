19e37edbfeaee34e8dd0d68f347941d6d26cd162
yogendra kunwar <n16104@std.it-college.ac.jp>
2016-08-25 12:06:19 +0900

    hihi

--- exam/exam.py
+++ exam/exam.py
@@ -1,73 +1,85 @@
 # 'hello, {name}!'と出力してください 。
 def hello(name):
-    pass
+    print('hello, {0}!'.format(name))
 
 
 # sentence の文字数を出力してください
 def length(sentence):
-    pass
+    print(len(sentence))
 
 
 # sentence の2文字目から5文字目まで(5文字目は含まない)を出力してください
 def slicing2to5(sentence):
-    pass
-
+    print(sentence[2:5])
 
 # number の符号を出力してください。ただし、0は'0'と出力してください
 def number_sign(number):
-    pass
-
-
+    if number > 0: return 1
+    else: return 0
 # number が素数なら'ok',そうでないなら'ng'と出力してください
 def prime_number(number):
-    pass
+    def is_prime(x):
+        if x < 2:
+            return'ng'
+        elif x == 2:
+            return True
+        elif x % 2 == 0 or x % number(x) == 0:
+            return'ng'
+        else:
+            return'ok'
 
 
 # 1からnumberまでの合計を出力してください
 def sum_from_1_to(number):
-    pass
+    def sum_list(l):
+        sum = 0
+        for x in l:
+            sum += x
+        return sum
 
 
 # numberの階乗(factorial)を出力してください
 def factorial(number):
-    pass
-
-
+    def factorial(n):
+        if n == 0:
+            return 1
+        else:
+            return n * factorial(n - 1)
 # リストdataの各要素(整数)を3乗した結果をリスト型として返してください
 def cubic_list(data):
-    pass
+
 
 
 # 底辺x,高さyの直角三角形(right angled triangle)の残り1つの辺の長さを返してください
 def calc_hypotenuse(x, y):
-    pass
+    print(x, y)
 
 
 # 底辺x,斜辺vの直角三角形の残り1つの辺の長さを返してください
 def calc_subtense(x, v):
-    pass
+    print(x, v)
 
 
 # 三辺の長さがそれぞれx,y,zの三角形の面積を返してください
 def calc_area_triangle(x, y, z):
-    pass
+    print(x, y, z)
 
 
 # 引数a,b,cを小数点以下2桁表示で空白切りで表示してください
 def point_two_digits(a, b, c):
-    pass
+    print(a, b, c)
 
 
 # リストdataの内容を小さい順でソートした結果を返してください
 def list_sort(data):
-    pass
+    print(data)
 
 
 # 文字列の並びを逆にしたものを返してください
 def reverse_string(sentence):
-    pass
+    print(sentence)
 
 
 # dateから2016年4月1日までの日数を返してください
 def days_from_date(point):
-    pass
+    print(point)
